f34254ecd6979663d374044ca6b13f53a0c53b3d
Colin Brayton <gringolalia@gmail.com>
2019-10-28 16:29:53 -0300

    deploy

---
+++ precache-manifest.ee3bf9734ff7442c1e822435c46e0547.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "9448a54c979340b3216d",
+    "url": "/reenact-furies/js/about.fdd22840.js"
+  },
+  {
+    "revision": "e16a4e8b692babdce635",
+    "url": "/reenact-furies/css/app.d9a39eba.css"
+  },
+  {
+    "revision": "e16a4e8b692babdce635",
+    "url": "/reenact-furies/js/app.2448a588.js"
+  },
+  {
+    "revision": "7a8529475ae67ae39aad",
+    "url": "/reenact-furies/css/chunk-vendors.0b84a99a.css"
+  },
+  {
+    "revision": "7a8529475ae67ae39aad",
+    "url": "/reenact-furies/js/chunk-vendors.c159c38b.js"
+  },
+  {
+    "revision": "89f0cfd0dfa7fb685406cb08a04aa01b",
+    "url": "/reenact-furies/img/biohazard.89f0cfd0.svg"
+  },
+  {
+    "revision": "93b45328c82e34dba3eac156d5600bdd",
+    "url": "/reenact-furies/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/reenact-furies/robots.txt"
+  },
+  {
+    "revision": "777d0214e2b505cea3bfd9e651b46fe9",
+    "url": "/reenact-furies/vuikit-0.8.5.zip"
+  },
+  {
+    "revision": "63cb184b6518339985b6bbf00929b73c",
+    "url": "/reenact-furies/vuikit/vuikit-icons.js"
+  },
+  {
+    "revision": "316c44c1887fc69bb4b6cde7b55ebff6",
+    "url": "/reenact-furies/vuikit/vuikit-icons.min.js"
+  },
+  {
+    "revision": "ceb373b53cc7222658305ee53fb848a7",
+    "url": "/reenact-furies/vuikit/vuikit.css"
+  },
+  {
+    "revision": "6e874f62583874671c2cd23e5c178cb8",
+    "url": "/reenact-furies/vuikit/vuikit.js"
+  },
+  {
+    "revision": "b59fda7e823d96b215b4f2aaa15f5832",
+    "url": "/reenact-furies/vuikit/vuikit.min.css"
+  },
+  {
+    "revision": "a91c95b3e036d1a603e5e2757a556bee",
+    "url": "/reenact-furies/vuikit/vuikit.min.js"
+  },
+  {
+    "revision": "777d0214e2b505cea3bfd9e651b46fe9",
+    "url": "/reenact-furies/vuikit/vuikit-0.8.5.zip"
+  }
+];
